8a18db6ea98391262cf0e496baf542c85700a670
Luan Lemos <ipsluan@icloud.com>
2023-06-08 22:11:12 -0300

    feat: add actions

--- src/components/FeedItem/index.js
+++ src/components/FeedItem/index.js
@@ -1,6 +1,15 @@
 import { useRef, useState } from "react";
-import { View, Text, StyleSheet, Pressable, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  Dimensions,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import { Video } from "expo-av";
+import { Ionicons } from "@expo/vector-icons";
 
 const { height: HeightScreen } = Dimensions.get("screen");
 
@@ -14,6 +23,29 @@ export function FeedItem({ data }) {
 
   return (
     <Pressable onPress={handlePlayer}>
+      <View style={styles.info}>
+        <Text style={styles.name}>{data?.name}</Text>
+        <Text numberOfLines={2} style={styles.description}>
+          {data?.description}
+        </Text>
+      </View>
+
+      <View style={styles.actions}>
+        <TouchableOpacity style={styles.actionsButton}>
+          <Ionicons name="heart" size={35} color="#fff" />
+          <Text style={styles.actionsText}>30.3K</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.actionsButton}>
+          <Ionicons name="chatbubble-ellipses" size={35} color="#fff" />
+          <Text style={styles.actionsText}>23</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.actionsButton}>
+          <Ionicons name="bookmark" size={35} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
       <Video
         ref={video}
         style={styles.video}
@@ -32,5 +64,41 @@ const styles = StyleSheet.create({
   video: {
     width: "100%",
     height: HeightScreen,
+  },
+  info: {
+    position: "absolute",
+    zIndex: 99,
+    left: 8,
+    padding: 8,
+    bottom: 110,
+  },
+  name: {
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 4,
+    textShadowColor: "rgba(0, 0, 0, 0.9)",
+    textShadowOffset: { width: -1, height: 1.5 },
+    textShadowRadius: 8,
+  },
+  description: {
+    color: "#fff",
+    marginRight: 24,
+    textShadowColor: "rgba(0, 0, 0, 0.20)",
+    textShadowOffset: { width: -1, height: 1.5 },
+    textShadowRadius: 8,
+  },
+  actions: {
+    position: "absolute",
+    zIndex: 99,
+    right: 10,
+    bottom: Platform.OS === "android" ? 120 : 170,
+    gap: 8,
+  },
+  actionsText: {
+    textAlign: "center",
+    color: "#fff",
+    textShadowColor: "rgba(0, 0, 0, 0.20)",
+    textShadowOffset: { width: -1, height: 1.5 },
+    textShadowRadius: 8,
   },
 });
